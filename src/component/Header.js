088154a954b602379b6056fb3b35598ec87ba8f2
Alka <rashinkaralka@gmail.com>
2024-06-21 15:17:22 +0530

    added class

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,8 @@ import Navbar from "./Navbar";
 export default function Header() {
    return (
       <div className="">
-         <header class="fixed top-0 w-full bg-white shadow-lg z-50 hidden md:block">
-            <nav class="container mx-auto p-4 flex justify-between items-center">
+         <header className="fixed top-0 w-full bg-white shadow-lg z-50 hidden md:block">
+            <nav className="container mx-auto p-4 flex justify-between items-center">
                <div className="md:text-4xl text-center font-bold cursor-pointer">SABYASACHI</div>
                <div className='my-menu-list lg:flex justify-center gap-5 mt-5'>
                   <ul className='flex items-center p-0 m-0' >
